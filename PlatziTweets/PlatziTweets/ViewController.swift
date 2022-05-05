e36de1321a4cf3da5b8baf121a98b792b723e624
Joaquin Segovia <joasegovia9427@gmail.com>
2022-05-04 22:40:48 -0300

    Validaciones para navegar

--- PlatziTweets/PlatziTweets/ViewController.swift
+++ PlatziTweets/PlatziTweets/ViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - References to UI
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButtonAction() {
+        //1. Get textFields inner values to validate
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        
+        if email == "email", password == "123" {
+            performSegue(withIdentifier: "home_segue", sender: nil)
+        }else{
+            print("Invalid credentials")
+        }
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
